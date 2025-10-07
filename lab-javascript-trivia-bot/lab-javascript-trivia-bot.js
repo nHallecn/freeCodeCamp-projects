8c49c42e9524e3875ef7ada1dcdf348f83542345
NJI HALLE CHO-NKWENTI <njihalle10@gmail.com>
2025-10-07 09:00:48 +0100

    Add files via upload

---
+++ lab-javascript-trivia-bot/lab-javascript-trivia-bot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Halle";
+const botLocation = "Yaounde";
+const favoriteLanguage = "JavaScript";
+
+const botNameLocation = "My name is " + botName + " and I live on " + botLocation +".";
+console.log(botNameLocation);
+
+const botfavoriteLanguage = "My favorite programming language is " + favoriteLanguage + ".";
+console.log(botfavoriteLanguage);
+
+let codingFact = favoriteLanguage + " is simple";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is a programming language.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " can be used to develop a website.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+ botName + " from " + botLocation + ".");
