92f2e17c4f275c7a57ae4aeef0ae1b40953114e6
NJI HALLE CHO-NKWENTI <njihalle10@gmail.com>
2025-10-10 08:11:12 +0100

    Add files via upload

---
+++ sentence maker/script.js
@@ -0,0 +1,11 @@
+let adjective = "tall", noun = "Man", verb="cold", place = "villa", adjective2 = "large", noun2 = "cheese";
+
+const firstStory = "Once upon a time, there was a(n) "+adjective+" " +noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+"."
+
+console.log("First story: "+firstStory);
+
+adjective = "fat", noun = "Girl", verb="play", place = "park", adjective2 = "small", noun2 = "slide";
+
+const secondStory = "Once upon a time, there was a(n) "+adjective+" " +noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+"."
+
+console.log("Second story: "+secondStory);
