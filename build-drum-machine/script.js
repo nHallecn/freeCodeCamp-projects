eaf4250f1c0622bb72d2cb8cf66a1ad143fe5e4c
NJI HALLE CHO-NKWENTI <njihalle10@gmail.com>
2025-11-21 08:28:42 +0100

    Update script.js

--- build-drum-machine/script.js
+++ build-drum-machine/script.js
@@ -6,58 +6,7 @@
 
 ** start of styles.css **
 
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 
-body{
-  background-color: #9a9797;
-  min-height: 100vh;
-  font-family: Arial, san-serif;
-}
-
-#drum-machine{
-  background-color: #cb1717;
-  margin: 30px auto;
-  width: 100%;
-  max-width: 600px;
-  padding: 20px 0 5px 0;
-  border-radius: 5px;
-  color: white;
-  border: 2px solid #201c1c;
-}
-
-#display{
-  font-size: 1.5rem;
-  font-weight: bold;
-  padding: 10px;
-}
-
-#pad-bank{
-  border: 2px solid #201c1c;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  text-align: center;
-  gap: 5px;
-  padding: 10px 0;
-  background-color: #000000;
-}
-
-.drum-pad{
-  background-color: #2c2c28;
-  padding: 50px 15px;
-  border-radius: 5px;
-  font-weight: bold;
-  font-size: 1.5rem;
-  transition: background ease;
-}
-
-.drum-pad:hover{
-  background-color: #cb1717;
-  color: #2c2c28;
-}
 
 ** end of styles.css **
 
